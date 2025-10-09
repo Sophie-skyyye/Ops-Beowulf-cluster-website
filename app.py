d212df314d4e5ab7db361313b7b30c37f1876cdf
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2025-10-09 16:19:54 +1300

    reverted back to some original names for syntax perposes

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 # to add more pages you will add this 
 # def about():
-#    return render_template("about.html")
+#    return render_templates("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
